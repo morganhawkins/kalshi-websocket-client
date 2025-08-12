e755b45d389c9603760932699bc0b92c26046760
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-08-12 17:17:44 -0400

    comment cleanup

--- src/channels/orderbook_updates.rs
+++ src/channels/orderbook_updates.rs
@@ -16,8 +16,6 @@ pub struct OrderbookSnapshotMessage {
     pub no: Vec<(u8, u64)>,
 }
 
-// {"market_ticker":"KXBTCD-25AUG1218-T120249.99","market_id":"7b08200f-0015-4bd0-94e1-2932826fa6c4","yes":[[3,80000],[4,8100],[5,20000],[18,1000],[19,1800],[20,503],[23,200]],"no":[[1,73590],[3,80000],[4,4431],[6,5400],[7,20000],[34,8000],[40,200],[66,200],[69,2250],[70,200],[73,800],[74,3]]}}
-
 #[derive(Deserialize, Debug)]
 pub struct OrderbookDelta {
     pub r#type: String,
@@ -35,5 +33,3 @@ pub struct OrderbookDeltaMessage {
     pub side: String,
     pub ts: String,
 }
-
-// {"type":"orderbook_delta","sid":1,"seq":3,"msg":{"market_ticker":"KXMLBGAME-25AUG12SDSF-SD","market_id":"dda543e6-bd0c-4275-8814-a17f6ab61762","price":51,"delta":-1,"side":"no","ts":"2025-08-12T21:00:32.539836022Z"}
